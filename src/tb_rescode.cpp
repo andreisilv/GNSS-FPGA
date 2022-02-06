012e9642b8bb0800166e2bf4226f8d2c9602b392
André Silva <andreisilva@tecnico.ulisboa.pt>
2022-02-06 00:59:21 +0000

    ......

--- src/tb_rescode.cpp
+++ src/tb_rescode.cpp
@@ -18,7 +18,7 @@ typedef struct ap_axis<O_BUS_b, 0, 0, 0> out_t;
 
 // --------------------------- IP  ----------------------------------
 
-void rescode(hls::stream<in_t> &strm_in, hls::stream<out_t> &strm_out, ap_fixed<64,16> *step, ap_fixed<64,16> *offset, ap_int<32> *len, ap_int<32> *codelen);
+void rescode(hls::stream<in_t> &strm_in, hls::stream<out_t> &strm_out);
 
 // --------------------------- Test Bench  ----------------------------------
 
@@ -43,66 +43,89 @@ int main()
 		printf("FILE ERROR\n");
 		exit(-1);
 	}
-
 	FILE *ftest[3] = {fcodE, fcodP, fcodL};
 
 	short int buffer[O_BUS_b/IO_TYPE_b];
 	short int res;
 	int err;
-	int h, i, j, bus_size;
 
-	hls::stream<in_t> strm_in;
+	ap_uint<1> stop_in, stop_out;
+
+	/* 1st */ ap_int<16> last = RESAMPLED_SIZE/(O_BUS_b/IO_TYPE_b) - 1;
+	/* 2nd */ ap_fixed<64,16> offset_0 = ap_fixed<64,16>(0 - 8*1023000*6.109482e-08);
+	/* 3rd */ ap_fixed<64,16> T_s = ap_fixed<64,16>(1023000*6.109482e-08);
+	/* 4th */ ap_int<8> code_displacement = 8; // 16 samples per chip
+	/* 5th */ ap_int<8> m_codes = 3; // early code, prompt code, late code
+
+    hls::stream<in_t> strm_in;
    	hls::stream<out_t> strm_out;
-	in_t in;
+
+	in_t arg, in;
 	out_t out;
 
+	/* ARGUMENTS */
+
+	arg.data = last;
+	arg.strb = 0xF;
+	arg.keep = 0xF;
+	arg.last = 1;
+	strm_in.write(arg); 	/* WRITE */
+
+	arg.data = offset_0.range(31,0);
+	arg.strb = 0xF;
+	arg.keep = 0xF;
+	arg.last = 1;
+	strm_in.write(arg); 	/* WRITE */
+
+	arg.data = offset_0.range(63,32);
+	arg.strb = 0xF;
+	arg.keep = 0xF;
+	arg.last = 1;
+	strm_in.write(arg); 	/* WRITE */
+
+	arg.data = T_s.range(31,0);
+	arg.strb = 0xF;
+	arg.keep = 0xF;
+	arg.last = 1;
+	strm_in.write(arg); 	/* WRITE */
+
+	arg.data.range(15,0) = T_s.range(47,32);
+	arg.data.range(23,16) = code_displacement;
+	arg.data.range(31,24) = m_codes;
+
+	arg.strb = 0xF;
+	arg.keep = 0xF;
+	arg.last = 1;
+	strm_in.write(arg); 	/* WRITE */
+
+	int h, i, j, bus_size;
+
+	/* Write Data  */
+
+	bus_size = I_BUS_b/IO_TYPE_b;
+
+	for(i = 0; i < CODE_SIZE; i += bus_size) {
+
+	   fread(buffer, sizeof(short int), bus_size, fcode);
+
+	   for(int j = 0; j < bus_size; j++)
+		   ((short int*) &(in.data))[j] = buffer[j];
+
+	   in.keep = 0xF;
+	   in.strb = i >= CODE_SIZE - bus_size ? 0x3 : 0xF;
+	   in.last = i >= CODE_SIZE - bus_size ? 1 : 0;
+
+	   strm_in.write(in);
+	}
+
+	/* HW */
+	rescode(strm_in, strm_out);
+
+	/* Test */
+
+	bus_size = O_BUS_b/IO_TYPE_b;
+
 	for(h = 0; h < 3; h++) {
-
-		/* Write Data  */
-
-		bus_size = I_BUS_b/IO_TYPE_b;
-
-		for(i = 0; i < CODE_SIZE; i += bus_size) {
-
-		   fread(buffer, sizeof(short int), bus_size, fcode);
-
-		   for(int j = 0; j < bus_size; j++)
-			   ((short int*) &(in.data))[j] = buffer[j];
-
-		   in.keep = 0xF;
-		   in.strb = i >= CODE_SIZE - bus_size ? 0x3 : 0xF;
-		   in.last = i >= CODE_SIZE - bus_size ? 1 : 0;
-
-		   strm_in.write(in);
-		}
-
-		rewind(fcode);
-
-		for(i = 0; i < CODE_SIZE; i += bus_size) {
-
-		   fread(buffer, sizeof(short int), bus_size, fcode);
-
-		   for(int j = 0; j < bus_size; j++)
-			   ((short int*) &(in.data))[j] = buffer[j];
-
-		   in.keep = 0xF;
-		   in.strb = i >= CODE_SIZE - bus_size ? 0x3 : 0xF;
-		   in.last = i >= CODE_SIZE - bus_size ? 1 : 0;
-
-		   strm_in.write(in);
-		}
-
-		ap_fixed<64,16> step = ap_fixed<64,16>(1023000*6.109482e-08);
-		ap_fixed<64,16> offset = ap_fixed<64,16>(0 - 8*1023000*6.109482e-08);
-		ap_int<32> len = RESAMPLED_SIZE;
-		ap_int<32> codelen = 1023;
-
-		/* Test */
-
-		rescode(strm_in, strm_out, &step, &offset, &len, &codelen);
-
-		bus_size = O_BUS_b/IO_TYPE_b;
-
 		if(!MUTE) printf("~~~~~~~~~~~~~~~~~~~~~~~~ CODE %d ~~~~~~~~~~~~~~~~~~~~~~~~\n", h);
 
 		for(i = 0; i < RESAMPLED_SIZE; i += bus_size) {
